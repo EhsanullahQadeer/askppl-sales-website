b87e30717a0fe8e8dc81cf99d5cc6e303708a3aa
Ehsanullah Qadeer <eehsanullah232@gmail.com>
2024-12-21 13:40:49 +0500

    edit changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,25 +37,19 @@ export default {
         lightLime: "#B9E567",
         secondaryLime: "#90B941",
 
-<<<<<<< HEAD
         etherealWhite: "#FFFFFF33",
         softWhite: "#FFFFFF80",
         ghostWhite: "#FFFFFF0A",
         lowWhite: "rgba(255, 255, 255, 0.04)",
         white50: "rgba(255, 255, 255, 0.50)",
-        green: "rgba(217, 217, 217, 0.10)"
-=======
+        green: "rgba(217, 217, 217, 0.10)",
         smokeGray: "#8E8D91",
         midnightAsh: "#1C1C1C",
         darkCharcoal: "#323232",
         hazyPearl: "rgba(255, 255, 255, 0.42)",
         mistWhite: "rgba(255, 255, 255, 0.10)",
         froastedWhite: "rgba(255, 255, 255, 0.16)",
-        etherealWhite: "rgba(255, 255, 255, 0.20)",
-        softWhite: "rgba(255, 255, 255, 0.50)",
-        ghostWhite: "rgba(255, 255, 255, 0.04)",
         gentleFog: "rgba(255, 255, 255, 0.08)",
->>>>>>> 6b465aac300bdcafae697cd8cacd49f0384ecb53
       },
       backgroundImage: {
         "clouded-white-gradient":
