4569603c048c9cdc379af19ca902725b1bd19d22
Ehsanullah Qadeer <eehsanullah232@gmail.com>
2024-12-22 17:28:08 +0500

    add the contact page properly

---
+++ src/assets/icons/Back.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Back = () => {
+  return (
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path d="M15 6C15 6 9 10.4189 9 12C9 13.5812 15 18 15 18" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+    </>
+  )
+}
+
+export default Back
+
