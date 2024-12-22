5bc236c74b22c72c3d4fa4da6ef6434c50306416
Ehsanullah Qadeer <eehsanullah232@gmail.com>
2024-12-22 22:49:37 +0500

    add the blogId page

--- src/app/(pages)/blog/[blogId]/page.tsx
+++ src/app/(pages)/blog/[blogId]/page.tsx
@@ -1,7 +1,72 @@
+import FacebookIcon from "@/assets/icons/FacebookIcon";
+import InstaIcon from "@/assets/icons/InstaIcon";
+import TwitterIcon from "@/assets/icons/TwitterIcon";
 import React from "react";
+import BlogCard from "../components/BlogCard";
+import Link from "next/link";
+import { blogsData } from "../data/data";
 
 const Page = () => {
-  return <div>BLOG DETAIL PAGE</div>;
+  return (
+    <div>
+      <div className=" w-full h-full py-24 ">
+        <div className="flex justify-center  items-center">
+          <div className="flex flex-col items-center justify-center  xl:w-3/5 px-5  ">
+            <div className="flex  flex-col gap-4">
+              <div className="text-hazyPearl gap-2 flex items-center font-medium ">
+                <span className="py-[5px] px-1.5 rounded-md font-medium text-sm bg-grey ">
+                  Community Building
+                </span>
+                <span className="text-sm text-softWhite ">•</span>
+                <span className="text-sm text-softWhite font-neueMontreal ">
+                  22 Nov 2024
+                </span>{" "}
+              </div>
+              <h2 className="sm:text-6xl text-3xl font-medium font-aeonik">
+                5 Proven Tips to Monetize Your Expertise with ASKPPL
+              </h2>
+            </div>
+            <div className=" w-full">
+              <div className="flex flex-wrap gap-4 justify-between w-full mt-12 items-center">
+                <span className="text-sm text-softWhite font-medium font-neueMontreal ">
+                  ASKPPL team
+                </span>
+                <div className="flex items-center  font-medium gap-5">
+                  <span>Share:</span>
+                  <div className="flex items-center gap-3">
+                    <span className="p-1.5 rounded-full border border-mistWhite">
+                      {" "}
+                      <FacebookIcon />
+                    </span>
+                    <span className="p-1.5 rounded-full border border-mistWhite">
+                      {" "}
+                      <InstaIcon />
+                    </span>
+                    <span className="p-1.5 rounded-full border border-mistWhite">
+                      {" "}
+                      <TwitterIcon />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="lg:px-[140px] px-5 py-[50px] flex flex-col md:gap-[50px] gap-[40px]">
+        <h2 className='text-3xl'>Related Blog</h2>
+        <div className="flex flex-wrap gap-y-10 gap-x-4">
+          {blogsData.slice(0, 3).map((blog) => {
+            return (
+              <Link href={`/blog/${blog.id}`} key={blog.id} className="flex-1">
+                <BlogCard {...{ blog }} />
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- src/assets/icons/TwitterIcon.tsx
+++ src/assets/icons/TwitterIcon.tsx
@@ -2,19 +2,9 @@ import React from "react";
 
 const TwitterIcon = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="17"
-      viewBox="0 0 16 17"
-      fill="none"
-    >
-      <path
-        d="M12.6 1H15.054L9.694 7.142L16 15.5H11.063L7.196 10.43L2.771 15.5H0.316L6.049 8.93L0 1H5.063L8.558 5.633L12.6 1ZM11.74 14.028H13.1L4.323 2.395H2.865L11.74 14.028Z"
-        fill="white"
-        fill-opacity="0.24"
-      />
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M13.6605 10.6217L20.17 3H18.6272L12.9765 9.61757L8.46105 3H3.25391L10.081 13.008L3.25391 21H4.79676L10.765 14.0109L15.5338 21H20.7409L13.6605 10.6217ZM11.548 13.0954L10.8563 12.099L5.35219 4.17H7.72176L12.1626 10.569L12.8543 11.5654L18.6285 19.884H16.2589L11.548 13.0954Z" fill="white"/>
+  </svg>
   );
 };
 
